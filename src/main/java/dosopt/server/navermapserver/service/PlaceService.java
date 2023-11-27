f82f3ae64d5f3198e53d5f968ef8f7f79fa66562
Kim-TaeUk <xodnr0130@naver.com>
2023-11-27 20:46:12 +0900

    [FEAT] 특정 장소의 검색 결과 간단 조회 구현 #2

--- src/main/java/dosopt/server/navermapserver/service/PlaceService.java
+++ src/main/java/dosopt/server/navermapserver/service/PlaceService.java
@@ -3,6 +3,8 @@ package dosopt.server.navermapserver.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import dosopt.server.navermapserver.api.dto.PlaceInfoResponse;
+import dosopt.server.navermapserver.domain.Place;
 import dosopt.server.navermapserver.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +13,9 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class PlaceService {
 	private final PlaceRepository placeRepository;
+
+	public PlaceInfoResponse getPlaceInfo(Long placeId) {
+		Place place = placeRepository.findByIdOrThrow(placeId);
+		return  PlaceInfoResponse.of(place);
+	}
 }
